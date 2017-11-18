5b3c6d9f13ce03dff57102363f3d859b193f5c1f
michaeljklein <michael14486@gmail.com>
2017-11-18 02:51:59 -0500

    ensure gen's classes are loaded

--- test/gen_test.rb
+++ test/gen_test.rb
@@ -11,6 +11,21 @@ class GenTest < Minitest::Test
     assert Gen.is_a?(Module)
   end
 
-end
+  def test_gen_code_defined
+    assert Gen::Code.is_a?(Class)
+  end
 
+  def test_gen_if_defined
+    assert Gen::If.is_a?(Class)
+  end
+
+  def test_gen_method_defined
+    assert Gen::Method.is_a?(Class)
+  end
+
+  def test_gen_predicate_defined
+    assert Gen::Predicate.is_a?(Class)
+  end
+
+end
 
